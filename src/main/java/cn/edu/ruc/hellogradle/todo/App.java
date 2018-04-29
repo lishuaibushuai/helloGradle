a0338d5d8ba4668a602118f9212d26d6b61ffc22
Lishuai <li.shuai@cgtn.com>
2018-04-30 03:23:55 +0800

    add logback

--- src/main/java/cn/edu/ruc/hellogradle/todo/App.java
+++ src/main/java/cn/edu/ruc/hellogradle/todo/App.java
@@ -1,5 +1,8 @@
 package cn.edu.ruc.hellogradle.todo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Scanner;
 
 /**
@@ -7,11 +10,14 @@ import java.util.Scanner;
  * @Date: Created on 2018/4/30
  */
 public class App {
+
+    private static Logger log = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args){
         int i = 0;
         Scanner scanner = new Scanner(System.in);
         while (++ i > 0){
-            System.out.println(i + ". place input todo item name:");
+            log.info(i + ". place input todo item name:");
             TodoItem item = new TodoItem(scanner.nextLine());
             System.out.println(item);
         }
